3044efa6ba5142316f69b34b59fffdbe7392ef96
ST1LL-1N-S1MUL4T10N <simonafonso4@gmail.com>
2025-03-22 17:13:12 +0100

    more scripts added

---
+++ scripts/dns_freedom/dns_freedom.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if the script is run as root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "You must run this script as root!"
+    exit 1
+fi
+
+# Display a thank you message
+echo "Thank you for using this script."
+echo "You will take back full control of your DNS in ... !"
+for i in 3 2 1; do
+    echo "$i..."
+    sleep 1
+
+done
+
+#Execute the script
+
+echo "nameserver 127.0.0.1" > /etc/resolv.conf
